a4fe6c09ee9d49ceb94faee1cccd051e62fbb8cb
Hugo <gamingburber@gmail.com>
2025-11-28 14:40:21 +0000

    update restart command

--- util_scripts/keysocket-restart.py
+++ util_scripts/keysocket-restart.py
@@ -1,9 +1,10 @@
+#!/usr/bin/env python3
 from subprocess import call
 
 call('echo Reloading PM2', shell=True)
-call('pm2 reload 0 keysocket', shell=True)
-call('pm2 status', shell=True)
+call('/usr/local/bin/pm2 reload 0 keysocket', shell=True)
+call('/usr/local/bin/pm2 status', shell=True)
 call('sudo nginx -t', shell=True)
+call('echo Restarting Nginx', shell=True)
+call('sleep 2s', shell=True)
 call('sudo systemctl restart nginx', shell=True)
-call('sudo systemctl reload nginx', shell=True)
-
