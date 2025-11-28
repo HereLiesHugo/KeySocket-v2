4702b49e6d030a68f0259c85fb482d5bd0ef6cd8
Hugo <gamingburber@gmail.com>
2025-11-28 14:42:13 +0000

    fix pm2 path I hope

--- util_scripts/keysocket-restart.py
+++ util_scripts/keysocket-restart.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
+import os
 from subprocess import call
 
-call('echo Reloading PM2', shell=True)
-call('/usr/local/bin/pm2 reload 0 keysocket', shell=True)
-call('/usr/local/bin/pm2 status', shell=True)
+# Get PM2 path
+pm2_path = os.popen('which pm2').read().strip()
+
+call(f'echo Reloading PM2', shell=True)
+call(f'{pm2_path} reload 0 keysocket', shell=True)
+call(f'{pm2_path} status', shell=True)
 call('sudo nginx -t', shell=True)
 call('echo Restarting Nginx', shell=True)
 call('sleep 2s', shell=True)
