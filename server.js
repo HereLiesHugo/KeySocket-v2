a6ec309163e18e1ea49f70ceedaebe36b19d8823
GamingBurber <84995264+HereLiesHugo@users.noreply.github.com>
2025-12-10 21:11:29 +0100

    cookie fix

--- server.js
+++ server.js
@@ -185,6 +185,7 @@ app.use((req, res, next) => {
 
 app.use(express.json({ limit: '200kb' }));
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser()); // Required for CSRF protection to read cookies
 
 // Static file serving with caching
 app.use('/lib', express.static('lib', {
